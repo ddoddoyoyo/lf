69b9f9d75e34e220cb41e1465477369a37cf21e2
hye yeon park <purely36@gmail.com>
2017-07-19 12:05:07 +0900

    img center & file 정리

--- ms/js/lf_main.js
+++ ms/js/lf_main.js
@@ -23,110 +23,94 @@ $(document).ready(function(){
 		});
 	});
 
-	slidingImg('sFront');
-	slidingImg('sSide');
-	slidingImg('sRear');
-	$("#content").onepage_scroll({
-		sectionContainer: "article",
-		loop: false,
-		updateURL: false,
-		responsiveFallback: false,
-		beforeMove : function(index){
-			//console.log(index);
+	$('#fullpage').fullpage({
+		verticalCentered: false,
+		'onLeave' :function(anchorLink, index){//beforePage
 			if(index ==3){
+				_slide = 'sFront';
 				beforePage('sFront');
 			}
 			else if(index == 5){
+				_slide = 'sSide';
 				beforePage('sSide');
 			}
 			else if(index == 7){
+				_slide = 'sRear';
 				beforePage('sRear');
 			}
+			//$('#fullpage .slidePage#'+ _slide +' .slideImgOver img').css('height', $('#fullpage .slidePage#'+ _slide +' .slideImg img').height());
 		},
-		afterMove : function(index){
+		'afterLoad': function(anchorLink, index){
 			if(index ==3){
-				afterPage('sFront');	
+				//_index = 1;
+				afterPage('sFront');
 			}
 			else if(index == 5){
+				//_index = 1;
 				afterPage('sSide');
 			}
 			else if(index == 7){
+				//_index = 1;
 				afterPage('sRear');
 			}
-		}
-	});//onepage_scroll
-	function slidingImg(pageId){
-		$('#content .slidePage#'+pageId+' .slideImg img').on("swipeleft",function(){
-			slideNext(pageId);
-		});
-		$('#content .slidePage#'+pageId+' .slideImg img').on("swiperight",function(){
-			slidePrev(pageId);
-		});
-		$('#content .slidePage#'+pageId+' .slideControler .nextImg').click(function(){
-			slideNext(pageId);
-		});
-		$('#content .slidePage#'+pageId+' .slideControler .prevImg').click(function(){
-			slidePrev(pageId);
-		});	
-	}
-	function beforePage(pageId){
-		$('#content .slidePage#'+pageId+' .imgTitle p').hide();
-		$('#content .slidePage#'+pageId+' .imgTitle p:nth-child(1)').show();
-		$('#content .slidePage#'+pageId+' .textbox p').hide();
-		$('#content .slidePage#'+pageId+' .textbox p:nth-child(1)').show();
-		$('#content .slidePage#'+pageId+' .slideImg').css({"width":slideWidth});
-		$('#content .slidePage#'+pageId+' .indicator img').attr({"src":"../images/button/indicator_non_active.png"});
-		$('#content .slidePage#'+pageId+' .indicator img:nth-child(1)').attr({"src":"../images/button/indicator_active.png"});
-		$('#content .slidePage#'+pageId+' .slideImg').css({"left":0});
-	}
-	function afterPage(pageId){
-		totalSlides = $('#content .slidePage#'+pageId+' .slideImg img').length / 2;
-		slideWidth = $('#content .slidePage#'+pageId+' .slideImg').width();
-		//console.log(slideWidth + "   " + totalSlides);
-		$('#content .slidePage#'+pageId+' .slideImg').css({"width":slideWidth * totalSlides});
-		$('#content .slidePage#'+pageId+' .slideImg img').css({"width":slideWidth});
-		pos=1;
-		//console.log(pos);
-	}
-	function slideNext(pageId){
-		if(pos >= totalSlides){
-			return false;
-		}
-		else{
-			pos++;
-			//console.log(pos);
-			slideNextImg(pageId,totalSlides,slideWidth,pos);			
-		}
-	}
-	function slidePrev(pageId){
-		if(pos <= 1){
-			return false;
-		}
-		else{
-			pos--;
-			slidePrevImg(pageId,totalSlides,slideWidth,pos);			
-		}
-	}
-	function slideNextImg(pageId,totalSlides,slideWidth,pos){
-		//console.log(pos);
-		$('#content .slidePage#'+pageId+' .slideImg').animate({"left":-(slideWidth * (pos-1))},500);
-		$('#content .slidePage#'+pageId+' .indicator img').attr({"src":"../images/button/indicator_non_active.png"});
-		$('#content .slidePage#'+pageId+' .indicator img:nth-child('+pos+')').attr({"src":"../images/button/indicator_active.png"});
-		$('#content .slidePage#'+pageId+' .imgTitle p').hide();
-		$('#content .slidePage#'+pageId+' .imgTitle p:nth-child('+pos+')').show();
-		$('#content .slidePage#'+pageId+' .textbox p').hide();
-				$('#content .slidePage#'+pageId+' .textbox p:nth-child('+pos+')').show();
-	}
+		},
+		// 'afterSlideLoad': function(direction,anchorLink,slideAnchor,slideIndex){
+		// 	console.log(_slide);
+		// 	console.log(_index);
+		// 	console.log(direction +'    ' +anchorLink+'    ' +slideAnchor+'    ' +slideIndex);
+		// 	if (direction == 'right') {
+		// 		//_index[_slide] = _index[_slide] + 1;
+		// 		console.log(1);
+		// 		slideNextImg(_slide, totalSlides, slideWidth, slideIndex);
+		// 	}
+		// 	if (direction == 'left') {
+		// 		//_index[_slide] = _index[_slide] - 1;
+		// 		slidePrevImg(_slide, totalSlides, slideWidth, slideIndex);
+		// 	}
+		// }
+	});//fullpage
 
-	function slidePrevImg(pageId,totalSlides,slideWidth,pos){
-		//console.log(pos);
-		$('#content .slidePage#'+pageId+' .slideImg').animate({"left":-(slideWidth * (pos-1))},500);
-		$('#content .slidePage#'+pageId+' .indicator img').attr({"src":"../images/button/indicator_non_active.png"});
-		$('#content .slidePage#'+pageId+' .indicator img:nth-child('+pos+')').attr({"src":"../images/button/indicator_active.png"});
-		$('#content .slidePage#'+pageId+' .imgTitle p').hide();
-		$('#content .slidePage#'+pageId+' .imgTitle p:nth-child('+pos+')').show();
-		$('#content .slidePage#'+pageId+' .textbox p').hide();
-		$('#content .slidePage#'+pageId+' .textbox p:nth-child('+pos+')').show();
-	}
-});
+	$('#fullpage .slidePage .imgTitle p').hide();
+	$('#fullpage .slidePage .imgTitle p:nth-child(1)').show();
+	$('#fullpage .slidePage .textbox p').hide();
+	$('#fullpage .slidePage .textbox p:nth-child(1)').show();
+});//ready
 
+var _slide;
+var _index = {'sFront':1, 'sSide':1, 'sRear':1};//fullpage.js
+function beforePage(pageId){
+	$('#fullpage .slidePage#'+pageId+' .slideImg').css({"width":slideWidth});
+}
+function afterPage(pageId){
+	totalSlides = $('#fullpage .slidePage#'+pageId+' .slideImg img').length;
+	slideWidth = $('#fullpage .slidePage#'+pageId+' .slideImg').width();
+	$('#fullpage .slidePage#'+pageId+' .slideImg img,#fullpage .slidePage#'+pageId+' .slideImgOver img').css({"width":slideWidth});
+	pos=1;
+}
+
+	
+function slideImgPre(pageId) {//fullpage.js
+	$('#fullpage .slidePage#'+pageId+' .slideImgOver').hide();
+}
+
+function slideNextImg(pageId,totalSlides,slideWidth,pos){//fullpage.js
+	//console.log(pos);
+	$('#fullpage .slidePage#'+pageId+' .slideImgOver').css({"left":-(slideWidth * (pos-1))}).show();
+	$('#fullpage .slidePage#'+pageId+' .indicator img').attr({"src":"../images/button/indicator_non_active.png"});
+	$('#fullpage .slidePage#'+pageId+' .indicator img:nth-child('+pos+')').attr({"src":"../images/button/indicator_active.png"});
+	$('#fullpage .slidePage#'+pageId+' .imgTitle p').hide();
+	$('#fullpage .slidePage#'+pageId+' .imgTitle p:nth-child('+pos+')').show();
+	$('#fullpage .slidePage#'+pageId+' .textbox p').hide();
+	$('#fullpage .slidePage#'+pageId+' .textbox p:nth-child('+pos+')').show();
+}
+
+function slidePrevImg(pageId,totalSlides,slideWidth,pos){//fullpage.js
+	//console.log(pos);
+	$('#fullpage .slidePage#'+pageId+' .slideImgOver').css({"left":-(slideWidth * (pos-1))}).show();
+	$('#fullpage .slidePage#'+pageId+' .indicator img').attr({"src":"../images/button/indicator_non_active.png"});
+	$('#fullpage .slidePage#'+pageId+' .indicator img:nth-child('+pos+')').attr({"src":"../images/button/indicator_active.png"});
+	$('#fullpage .slidePage#'+pageId+' .imgTitle p').hide();
+	$('#fullpage .slidePage#'+pageId+' .imgTitle p:nth-child('+pos+')').show();
+	$('#fullpage .slidePage#'+pageId+' .textbox p').hide();
+	$('#fullpage .slidePage#'+pageId+' .textbox p:nth-child('+pos+')').show();
+}
