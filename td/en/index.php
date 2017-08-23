1ccabfe35336ef5d949abf13669497eb0efec0fc
hye yeon park <purely36@gmail.com>
2017-08-23 17:42:58 +0900

    vr test remove

--- td/en/index.php
+++ td/en/index.php
@@ -28,31 +28,7 @@
 		<div id="wrap">
 			<div id="contBox">
 			<section data-role="page" id="intro" class="container">
-				<div class="color_ex">
-					<div class="color_list">
-						<img src="../images/BG.png">  
-						<ul id="btn_ex_color_wrap">
-							<li id="btn_ex_color01"><a class="on" href="javascript:;"><img src="../images/01_over.png"></a></li>
-							<li id="btn_ex_color02"><a href="javascript:;"><img src="../images/02_defalt.png"></a></li>
-							<li id="btn_ex_color03"><a href="javascript:;"><img src="../images/03_defalt.png"></a></li>
-							<li id="btn_ex_color04"><a href="javascript:;"><img src="../images/04_defalt.png"></a></li>
-							<li id="btn_ex_color05"><a href="javascript:;"><img src="../images/05_defalt.png"></a></li>
-							<li id="btn_ex_color06"><a href="javascript:;"><img src="../images/06_defalt.png"></a></li>
-							<li id="btn_ex_color07"><a href="javascript:;"><img src="../images/07_defalt.png"></a></li>
-							<li id="btn_ex_color08"><a href="javascript:;"><img src="../images/08_defalt.png"></a></li>
-						</ul>
-					</div>
-
-					<div class="color_ex_img">
-						<img src="../VR/AE_HEV_Polar_White/000.png" class="reel" id="ex_color" data-images="../VR/AE_HEV_Polar_White/###.png" data-frames="35" data-frame="9" data-rows="1" data-row="1">
-					</div>
-					<div class="color_ex_img_back">
-						<img src="../images/002_01.png" id="back_img">
-					</div>
-					<div class="color_name">
-						
-					</div>
-				</div>
+				<input type="date">
 			</section>					
 			</div>
 		<!-- <a href="#page11">이동!!!!!!!!!</a> -->
--- td/en/main.php
+++ td/en/main.php
@@ -27,31 +27,7 @@
 		<div id="wrap">
 			<div id="contBox">
 			<section data-role="page" id="intro" class="container">
-				<div class="color_ex">
-					<div class="color_list">
-						<img src="images/BG.png">  
-						<ul id="btn_ex_color_wrap">
-							<li id="btn_ex_color01"><a class="on" href="javascript:;"><img src="images/01_over.png"></a></li>
-							<li id="btn_ex_color02"><a href="javascript:;"><img src="images/02.png"></a></li>
-							<li id="btn_ex_color03"><a href="javascript:;"><img src="images/03.png"></a></li>
-							<li id="btn_ex_color04"><a href="javascript:;"><img src="images/04.png"></a></li>
-							<li id="btn_ex_color05"><a href="javascript:;"><img src="images/05.png"></a></li>
-							<li id="btn_ex_color06"><a href="javascript:;"><img src="images/06.png"></a></li>
-							<li id="btn_ex_color07"><a href="javascript:;"><img src="images/07.png"></a></li>
-							<li id="btn_ex_color08"><a href="javascript:;"><img src="images/08.png"></a></li>
-						</ul>
-					</div>
-
-					<div class="color_ex_img">
-						<img src="VR/AE_HEV_Polar_White/000.png" width="940" height="462" class="reel" id="ex_color" data-images="VR/AE_HEV_Polar_White/###.png" data-frames="35" data-frame="9" data-rows="1" data-row="1">
-					</div>
-					<div class="color_ex_img_back">
-						<img src="images/002_01.png" id="back_img">
-					</div>
-					<div class="color_name">
-						
-					</div>
-				</div>
+				
 			</section>					
 			</div>
 		<!-- <a href="#page11">이동!!!!!!!!!</a> -->
