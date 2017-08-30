bf70d8be829c3ef54567562fa0d32d604405237a
hye yeon park <purely36@gmail.com>
2017-08-30 12:13:25 +0900

    application date update

--- td/en/application.php
+++ td/en/application.php
@@ -80,21 +80,71 @@
 							<div>
 								<label for="bYear">DATE <span>(M / D / Y / T)</span></label>
 								<select name="" id="bMonth" class="bSelect">
-									<option value="">08</option>
+									<option value="01">1</option>
+									<option value="02">2</option>
+									<option value="03">3</option>
+									<option value="04">4</option>
+									<option value="05">5</option>
+									<option value="06">6</option>
+									<option value="07">7</option>
+									<option value="08">8</option>
+									<option value="09">9</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
 								</select>
 								<select name="" id="bDay" class="bSelect">
-									<option value="">29</option>
+									<option value="01">1</option>
+									<option value="02">2</option>
+									<option value="03">3</option>
+									<option value="04">4</option>
+									<option value="05">5</option>
+									<option value="06">6</option>
+									<option value="07">7</option>
+									<option value="08">8</option>
+									<option value="09">9</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
+									<option value="13">13</option>
+									<option value="14">14</option>
+									<option value="15">15</option>
+									<option value="16">16</option>
+									<option value="17">17</option>
+									<option value="18">18</option>
+									<option value="19">19</option>
+									<option value="20">20</option>
+									<option value="21">21</option>
+									<option value="22">22</option>
+									<option value="23">23</option>
+									<option value="24">24</option>
+									<option value="25">25</option>
+									<option value="26">26</option>
+									<option value="27">27</option>
+									<option value="28">28</option>
+									<option value="29">29</option>
+									<option value="30">30</option>
+									<option value="31">31</option>
 								</select>
 								<select name="" id="bYear" class="bSelect">
 									<option value="">2017</option>
 									<option value="">2018</option>
 								</select>
 								<select name="" id="bHour" class="bSelect">
-									<option value="">15</option>
+									<option value="09">09</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
+									<option value="13">13</option>
+									<option value="14">14</option>
+									<option value="15">15</option>
+									<option value="16">16</option>
+									<option value="17">17</option>
 								</select>
 								<span>:</span>
 								<select name="" id="bMin" class="bSelect">
-									<option value="">00</option>
+									<option value="00">00</option>
+									<option value="30">30</option>
 								</select>
 							</div>
 							<div><label for="bookName">NAME</label><input type="text" id="bookName"></div>
