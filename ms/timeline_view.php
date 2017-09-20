a6d7fbe5767153d645f4981e72ccf1538f8bbe8a
hye yeon park <purely36@gmail.com>
2017-09-20 10:52:57 +0900

    timeline server + a

--- ms/timeline_view.php
+++ ms/timeline_view.php
@@ -66,17 +66,24 @@
 					<article class="article">
 						<div class="photo">
 							<?php if($ls["LMS_CON_TITLE_IMG"]) { ?>
-								<img src="<?=$IMG_URL?>/hyundai/lf/<?=$ls["LMS_CON_TITLE_IMG"]?>" alt="">
+								<!-- <img src="<?=$IMG_URL?>/hyundai/lf/<?=$ls["LMS_CON_TITLE_IMG"]?>" alt=""> -->
+								<div class="img" style="background-image : url('<?=$IMG_URL?>/hyundai/lf/<?=$ls["LMS_CON_TITLE_IMG"]?>');"></div>
 							<?php } else { ?>
-								<img src="" alt="">
+								<!-- <img src="" alt=""> -->
 							<?php } ?>
 						</div>
 						<div class="profile_wrap">
 							<div class="userPix_wrap">
 								<?php if($ls["LMS_IMAGE"]) { ?>
-									<div class="userPix"><img src="<?=$IMG_URL?>/hyundai/member/<?=$ls["LMS_IMAGE"]?>" alt=""></div>
+									<div class="userPix">
+										 <!-- <img src="<?=$IMG_URL?>/hyundai/member/<?=$ls["LMS_IMAGE"]?>" alt=""> -->
+										<div class="img" style="background-image : url('<?=$IMG_URL?>/hyundai/member/<?=$ls["LMS_IMAGE"]?>');"></div>
+									</div>
 								<?php } else { ?>
-									<div class="userPix"><img src="images/timeline/profile_basic_@3x.png" alt=""></div>
+									<div class="userPix">
+										<!-- <img src="images/timeline/profile_basic_@3x.png" alt=""> -->
+										<div class="img" style="background-image : url('images/timeline/profile_basic_@3x.png');"></div>
+									</div>
 								<?php } ?>
 							</div>
 							<div class="userProfile">
